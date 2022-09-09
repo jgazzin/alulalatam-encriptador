269631c5ec7b41becaa902380a2d8598440ea5e4
Julieta Gazzin <j.gazzin@gmail.com>
2022-09-09 02:14:27 -0300

    version alura terminada

--- java.js
+++ java.js
@@ -1,15 +1,9 @@
-const inputTexto = document.querySelector (toLowerCase(".texto-entrada"));
-const outputMensaje = document.querySelector ("texto-salida");
+const inputTexto = document.querySelector(".texto-entrada");
+const outputMensaje = document.querySelector(".texto-salida");
 
 
 /* FUNCIONES */
-/*
-Includes()
-toLowerCase()
-replaceAll()
-navigator.clipboard
-Element.style
-*/
+
 // La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
 // La letra "a" es convertida para "ai"
@@ -17,9 +11,11 @@ Element.style
 // La letra "u" es convertida para "ufat"
 
 
-function encriptar () {
-    var arrayRemplazar = ["a", "e", "i", "o", "u"];
-    var arrayRemplazantes = ["imes", "ai", "ober", "ufat"]; 
+function encriptar (stringAencriptar) {
+   
+    var arrayRemplazar = ["e", "i", "a", "o", "u"];
+    var arrayRemplazantes = ["enter", "imes","ai", "ober", "ufat"]; 
+    stringAencriptar = stringAencriptar.toLowerCase();
 
     let x = "";
     let y = "";
@@ -27,18 +23,59 @@ function encriptar () {
     for (let index = 0; index < arrayRemplazar.length; index++) {
         x = arrayRemplazar [index];
         y = arrayRemplazantes [index];
-        outputMensaje = outputMensaje.replace(x, y);
+        stringAencriptar = stringAencriptar.replaceAll(x, y);
 
-        /* control:
-        document.write(x);
-        document.write("<br>");
-        document.write(y);
-        document.write("<br>");
-        document.write(outputMensaje);
-        document.write("<br>"); */
     }
 
-    return outputMensaje;
+    return stringAencriptar;
 }
 
-encriptar.onclick = encriptar;
+function desencriptar (stringAencriptar) {
+   
+    var arrayRemplazar = ["a", "e", "i", "o", "u"];
+    var arrayRemplazantes = ["ai", "enter","imes", "ober", "ufat"]; 
+    stringAencriptar = stringAencriptar.toLowerCase();
+
+    let x = "";
+    let y = "";
+
+    for (let index = 0; index < arrayRemplazar.length; index++) {
+        x = arrayRemplazar [index];
+        y = arrayRemplazantes [index];
+        stringAencriptar = stringAencriptar.replaceAll(y, x);
+
+    }
+
+    return stringAencriptar;
+}
+
+function botonEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    outputMensaje.value = textoEncriptado;
+    
+    /* apagar la imagen de sinMensaje*/
+    document.getElementById("salidaMensaje").style.display = "block";
+    document.getElementById("sinMensaje").style.display = "none";
+
+    /* limpiar mensaje original */
+    inputTexto.value = "";
+}
+
+function botonDesencriptar() {
+    const textoDesencriptado = desencriptar(inputTexto.value);
+    outputMensaje.value = textoDesencriptado;
+    
+    /* apagar la imagen de sinMensaje*/
+    document.getElementById("salidaMensaje").style.display = "block";
+    document.getElementById("sinMensaje").style.display = "none";
+
+    /* limpiar mensaje original */
+    inputTexto.value = "";
+}
+
+
+function copiar() {
+    outputMensaje.select();
+    navigator.clipboard.writeText(outputMensaje.value);
+    outputMensaje.value = "";
+}
